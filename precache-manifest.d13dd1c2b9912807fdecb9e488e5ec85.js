060a788e66261722c0e7809623f157395fd092dd
Hardikkhanesa <hkhanesa@gmail.com>
2020-05-30 10:28:13 +0530

    Updates

---
+++ precache-manifest.d13dd1c2b9912807fdecb9e488e5ec85.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "163419bd6e0678ee45ab",
+    "url": "https://avi-final.web.app/static/js/14.8542b144.chunk.js"
+  },
+  {
+    "revision": "15c591d13165dc4dac1e",
+    "url": "https://avi-final.web.app/static/js/0.da0831c6.chunk.js"
+  },
+  {
+    "revision": "6b530dc81a1e45d0a9c7",
+    "url": "https://avi-final.web.app/static/js/2.36df00e5.chunk.js"
+  },
+  {
+    "revision": "4cd0d106fbbd473e6ef6e0b995bac6d7",
+    "url": "https://avi-final.web.app/static/media/tick.4cd0d106.png"
+  },
+  {
+    "revision": "fd76d438e62af9a87dca",
+    "url": "https://avi-final.web.app/static/js/main.03e2dd30.chunk.js"
+  },
+  {
+    "revision": "3af23a48ba9e6885a495",
+    "url": "https://avi-final.web.app/static/js/runtime~main.fff1c397.js"
+  },
+  {
+    "revision": "0fc7c91553f42bde865a",
+    "url": "https://avi-final.web.app/static/js/5.b4d68034.chunk.js"
+  },
+  {
+    "revision": "740738c5100462ba0393",
+    "url": "https://avi-final.web.app/static/js/6.134952ec.chunk.js"
+  },
+  {
+    "revision": "f5c13610969fcc1d8f4f",
+    "url": "https://avi-final.web.app/static/js/7.b9a423f2.chunk.js"
+  },
+  {
+    "revision": "7313bd5def068c650925",
+    "url": "https://avi-final.web.app/static/js/8.f41fca9b.chunk.js"
+  },
+  {
+    "revision": "a8e236c95c1b4305f52c",
+    "url": "https://avi-final.web.app/static/js/9.6315942a.chunk.js"
+  },
+  {
+    "revision": "e2df628ef12f9f4b097f",
+    "url": "https://avi-final.web.app/static/js/10.ad2b2e43.chunk.js"
+  },
+  {
+    "revision": "e29a2456fff131a5791e",
+    "url": "https://avi-final.web.app/static/js/11.7dd7be64.chunk.js"
+  },
+  {
+    "revision": "efb6cdfc244b0b4d8855",
+    "url": "https://avi-final.web.app/static/js/12.c02dc334.chunk.js"
+  },
+  {
+    "revision": "b54aecde84cf85d01bfe",
+    "url": "https://avi-final.web.app/static/js/13.b1a85f8c.chunk.js"
+  },
+  {
+    "revision": "f8211f6763fe5cc9ce29",
+    "url": "https://avi-final.web.app/static/js/1.0e998e91.chunk.js"
+  },
+  {
+    "revision": "b175e1c4790112fe609a0f3279e8535d",
+    "url": "https://avi-final.web.app/static/media/logo_200.b175e1c4.png"
+  },
+  {
+    "revision": "8cdcaf052e2193b93e46",
+    "url": "https://avi-final.web.app/static/js/15.f88e4415.chunk.js"
+  },
+  {
+    "revision": "80d4a4e5af9b6cf3882e4a34c5497542",
+    "url": "https://avi-final.web.app/static/media/sidebar-4.80d4a4e5.jpg"
+  },
+  {
+    "revision": "fc9cf2b87fb924cdf4ad",
+    "url": "https://avi-final.web.app/static/js/16.b94344c3.chunk.js"
+  },
+  {
+    "revision": "9b864b3680f372ff9ae02649fbc44838",
+    "url": "https://avi-final.web.app/static/media/reactlogo.9b864b36.png"
+  },
+  {
+    "revision": "5763dbb38c5cf0cac41e",
+    "url": "https://avi-final.web.app/static/js/17.ef647c78.chunk.js"
+  },
+  {
+    "revision": "e89ab72eca9560a448a4",
+    "url": "https://avi-final.web.app/static/js/18.b09c5cb3.chunk.js"
+  },
+  {
+    "revision": "a6a92c4d85c62d06d54f",
+    "url": "https://avi-final.web.app/static/js/19.fbd1e4b9.chunk.js"
+  },
+  {
+    "revision": "30ac849e829a76230eb4",
+    "url": "https://avi-final.web.app/static/js/20.d4cfe13f.chunk.js"
+  },
+  {
+    "revision": "09b7ee1b2434a3d98760",
+    "url": "https://avi-final.web.app/static/js/21.15e233db.chunk.js"
+  },
+  {
+    "revision": "fd76d438e62af9a87dca",
+    "url": "https://avi-final.web.app/static/css/main.0ca752dd.chunk.css"
+  },
+  {
+    "revision": "5763dbb38c5cf0cac41e",
+    "url": "https://avi-final.web.app/static/css/17.f929d69d.chunk.css"
+  },
+  {
+    "revision": "fc9cf2b87fb924cdf4ad",
+    "url": "https://avi-final.web.app/static/css/16.f929d69d.chunk.css"
+  },
+  {
+    "revision": "8cdcaf052e2193b93e46",
+    "url": "https://avi-final.web.app/static/css/15.f929d69d.chunk.css"
+  },
+  {
+    "revision": "48fb09f8552afa098ee14c55d6921ae1",
+    "url": "https://avi-final.web.app/index.html"
+  }
+];
